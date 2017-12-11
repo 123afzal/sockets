00e4702e42fa29f2e542fe81a531cdd33869ee3d
Syed Afzal <syed.afzal@codenvoi.com>
2017-12-11 12:27:48 +0500

    nickname

--- server.js
+++ server.js
@@ -6,35 +6,33 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 4200;
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection',function (socket) {
-    socket.on('send-nickname', function(nickname) {
+io.on('connection', function (socket) {
+    socket.on('send-nickname', function (nickname) {
         socket.nickname = nickname;
-        console.log('a user connected',socket.nickname);
-        socket.broadcast.emit('new user',socket.nickname);
+        console.log('a user connected', socket.nickname);
+        socket.broadcast.emit('new user', socket.nickname);
     });
 
 
-
     //disconnect whenever refresh
-    socket.on('disconnect', function(){
+    socket.on('disconnect', function () {
         console.log('user disconnected');
     });
 
     //logging msgs event
-    socket.on('chat message', function(msg){
+    socket.on('chat message', function (msg) {
         console.log('message of: ' + socket.nickname + msg);
 
         //In order to send an event to everyone
-        io.emit('chat message', msg);
+        io.emit('chat message', msg, socket.nickname);
     });
 });
 
 
-
-http.listen(port, function(){
+http.listen(port, function () {
     console.log('listening on *:' + port);
 });
